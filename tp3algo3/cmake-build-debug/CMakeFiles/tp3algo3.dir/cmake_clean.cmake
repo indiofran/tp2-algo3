c2f2e94745a8066e77dc58c174ddd72bcee487c2
Francisco Nicolas Gomez Salaverri <fsalaverri@ws22.labo2>
2019-05-10 19:49:53 -0300

    add cmake and c++ code

---
+++ tp3algo3/cmake-build-debug/CMakeFiles/tp3algo3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp3algo3.dir/main.cpp.o"
+  "tp3algo3.pdb"
+  "tp3algo3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tp3algo3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tp3algo3/cmake-build-debug/CMakeFiles/tp3algo3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tp3algo3.
+# This may be replaced when dependencies are built.
